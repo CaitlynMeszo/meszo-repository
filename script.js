f5b73cb3c19f4a4ebcc3df7b92d52e500f72ce38
Caitlyn <meszaros.caitlyn@gmail.com>
2021-05-28 19:24:42 -0400

    added event listener to make mouse over green and mouse leave white on h1(header) w/ js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ h2.textContent = "Hey, I'm Caitlyn.";
 document.querySelector('body').appendChild(h2);
 
 h2.style.color = "#FFFFFF";
+
+document.getElementById("header")
+    header.addEventListener('mouseover', function(){
+         header.style.color = "#6AEF8C";});
+
+    header.addEventListener('mouseleave', function() {
+        header.style.color = "#FFFFFF";});
